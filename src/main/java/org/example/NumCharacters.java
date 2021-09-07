a939c56a5bc28ffc840bb2f75ceac57db79299b5
Daniela <danigomezdugan@gmail.com>
2021-09-07 16:45:07 -0400

    Updating file

--- src/main/java/org/example/NumCharacters.java
+++ src/main/java/org/example/NumCharacters.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 
 /**
  * UCF COP3330 Fall 2021 Assignment 1 Solution
- *Copyright 2021 daniela gomez-dugan
+ *Copyright 2021 Daniela Gomez-Dugan
  */
 public class NumCharacters
 {
     public static void main( String[] args )
     {
         Scanner input = new Scanner(System.in);
-        System.out.println("What is the input string?");
+        System.out.print("What is the input string? ");
         String word = input.nextLine();
         while (word.length() == 0) {
-            System.out.println("I am sorry. Please enter a valid string. What is the input string?");
+            System.out.print("I am sorry. Please enter a valid string. What is the input string?");
             word = input.nextLine();
         }
         System.out.println(word + " has " + word.length() + " characters.");
