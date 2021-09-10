5c5bc34bee136623735032691ac62694345844b1
Daniela <danigomezdugan@gmail.com>
2021-09-09 20:47:43 -0400

    Updating file

--- src/main/java/org/example/NumCharacters.java
+++ src/main/java/org/example/NumCharacters.java
@@ -10,12 +10,16 @@ public class NumCharacters
     public static void main( String[] args )
     {
         Scanner input = new Scanner(System.in);
+
         System.out.print("What is the input string? ");
         String word = input.nextLine();
+
+        // Checking for empty strings
         while (word.length() == 0) {
             System.out.print("I am sorry. Please enter a valid string. What is the input string?");
             word = input.nextLine();
         }
+
         System.out.println(word + " has " + word.length() + " characters.");
     }
 }
